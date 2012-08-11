ef6b33785a4a28c14fb1bb579837dbee9f5d8755
Anil Server <anilashanbhag@gmail.com>
2012-08-11 11:59:11 -0500

    temp commit

--- box_app/views.py
+++ box_app/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 import boxdotnet as box
 from apikeys import *
-from django.shortcuts import redirect
 
 def box_addaccount(request):
     boxClient = box.BoxDotNet()
     return redirect( boxClient.auth_url( BOX_API_KEY ) )
 
 def box_oauthcallback(request):
+    request.GET["ticket"]
+    request.GET["auth_token"]
     return HttpResponse("hello u")
--- google_app/views.py
+++ google_app/views.py
@@ -1,20 +1,12 @@
+import os
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-import boxdotnet as box
-from apikeys import *
 from django.shortcuts import redirect
-CLIENTSECRETS_LOCATION = os.path.join(static_folder,'CLIENT_SECRETS.JSON')
+CLIENTSECRETS_LOCATION = os.path.join('static','CLIENT_SECRETS.JSON')
 REDIRECT_URI = 'http://wncc.webfactional.com/google/oauthcallback'
-
+SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile',]
 def google_addaccount(request):
-	low = OAuth2WebServerFlow(
-              # Visit https://code.google.com/apis/console to
-              # generate your client_id, client_secret and to
-              # register your redirect_uri.
-              client_id='640541804881-qn8sn3la5ag395ptr341mtebf9s49ru2.apps.googleusercontent.com',
-              client_secret='NAU1FwwH_m8Hi50APEfonoQZ',
-              scope=SCOPES,
-              user_agent='buzz-cmdline-sample/1.0')
+    flow = OAuth2WebServerFlow(client_id='640541804881-qn8sn3la5ag395ptr341mtebf9s49ru2.apps.googleusercontent.com',client_secret='NAU1FwwH_m8Hi50APEfonoQZ',scope=SCOPES,user_agent='buzz-cmdline-sample/1.0')
     callback = 'http://wncc.webfactional.com/google/oauthcallback'
     authorize_url = flow.step1_get_authorize_url(callback)
     return redirect(authorize_url)
