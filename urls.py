f866c306d1c4d6c44908a16d3dd45ec39d6a3cec
Ashwin Paranjape <ashwing.2005@gmail.com>
2012-08-11 16:55:10 +0530

    addurl link for dropbox

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from views import home
+from DropboxClient import DropboxClient
 import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -18,4 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     ('^$', home),
     (r'^static/(?P<path>.*)', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
+    (r'^addaccount/dropbox$', 'DropboxClient.register'),)
 )
