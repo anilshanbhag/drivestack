ef1f907d77a0534abf8adaf80b0179922a46d844
Anil Server <anilashanbhag@gmail.com>
2012-08-11 10:14:06 -0500

    temp commit

--- dropbox_app/views.py
+++ dropbox_app/views.py
@@ -1,14 +1,15 @@
 # Create your views here.
-#from DropBoxClient import DropBoxClient
+from DropBoxClient import DropBoxClient
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def dropboxinterface(request,type):
     if type == "register":
-        return HttpResponse(type)
+        return DropBoxClient.register(request)
+        
+        #return HttpResponse(type)
 
 
-        #return DropBoxClient.register(request)
 
         
         
