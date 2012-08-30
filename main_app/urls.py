a04dc8319fff753ba8f10ee103f1059da5709cef
Anil Server <anilashanbhag@gmail.com>
2012-08-30 06:50:58 -0500

    Adding logout

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,22 +1,18 @@
 from django.conf.urls.defaults import *
-from main_app.views import homepage, home, upload, home_saket, crosssharerequest, crosssharemanifest
-from dropbox_app.views import dropboxinterface
-from box_app.views import box_addaccount, box_oauthcallback, box_download
-from google_app.views import google_addaccount, google_oauthcallback, refresh_google_token
+import main_app.views
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^$', homepage),
-    (r'^home$', home),
 
-    (r'^homes$', home_saket),
-    (r'^share$', crosssharerequest),
-    (r'^shareaccept$', crosssharemanifest),
-    (r'^upload$', upload),
-    (r'^dropbox/([a-z]*)$', dropboxinterface),
+    (r'^$', main_app.views.homepage),
+    (r'^home$', main_app.views.home),
+    (r'^share$', main_app.views.crosssharerequest),
+    (r'^shareaccept$', main_app.views.crosssharemanifest),
+    (r'^upload$', main_app.views.upload),
+    (r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
     (r'^box/', include('box_app.urls')),
     (r'^google/', include('google_app.urls')),
